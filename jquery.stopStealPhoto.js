91e7271ba4a127a4013432f54460d559f8aad434
Andrey Novikov <djmansys@narod.ru>
2016-08-29 11:42:06 +0300

    Public

---
+++ jquery.stopStealPhoto.js
@@ -0,0 +1,59 @@
+(function($) {
+  $.fn.stopStealPhoto = function(options) {
+
+     // default settings
+     var settings = $.extend({
+     	message : 'This image is protected',
+          delay: 600,
+          customClass: undefined,
+          cursorOffset: 14
+     }, options);
+
+     var timer;
+     var elem = this;
+
+     this.on('contextmenu', function(event){
+     	event.preventDefault();
+
+          currentElement = event.currentTarget;
+
+     	hintPositionX = event.clientX + settings.cursorOffset;
+     	hintPositionY = event.clientY + settings.cursorOffset;
+
+          window.clearTimeout(timer);
+     	$('.ssph-hint').remove();
+
+     	newId = Math.floor(Date.now());
+
+     	hint = $('<div class="ssph-hint" id="ssph-'+newId+'" />');
+
+          if(settings.customClass) hint.addClass(settings.customClass);
+
+     	hint.css({
+     		'position' : 'absolute',
+     		'left' : hintPositionX,
+     		'top' : hintPositionY,
+     		'z-index' : 100,
+     		'background-color' : 'rgba(0,0,0,0.75)',
+     		'color' : '#fff',
+     		'font-size' : '11px',
+     		'padding' : '4px',
+     		'font-family' : 'sans-serif',
+     		'border-radius' : '4px',
+     	})
+
+          // if data attribute exist in current element -> take text from it
+          var dataText = $(currentElement).attr('data-ssph');
+          hintText = (dataText ? dataText : settings.message);
+     	hint.text(hintText);
+
+     	$('body').append(hint);
+
+     	timer = window.setTimeout(function(){
+     		$('#ssph-'+newId).fadeOut();
+     	}, settings.delay);
+
+     })
+
+  };
+})(jQuery);
